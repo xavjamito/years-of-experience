bf1479adad9325c3a2e62b41edf4b67985ad2139
Xavier Paolo Jamito <xavierjamito@gmail.com>
2020-06-22 16:39:16 +0800

    Initial commit for complete project

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+  app.get('*', (res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
