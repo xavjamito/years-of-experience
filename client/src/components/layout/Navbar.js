a0bd20b889f7e37fdda462f6e4085c50f17c3c8e
Xavier Paolo Jamito <xavierjamito@gmail.com>
2020-06-22 17:00:22 +0800

    change navbar label

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
+        <Link to='/profiles'>Profiles</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
